648467f7a33d5802b40dd14112cb8068a1d7e314
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-08-27 08:34:43 +0300

    modified auth

--- manage.py
+++ manage.py
@@ -24,3 +24,33 @@ def test():
 
 if __name__ == "__main__":
     manager.run()
+    
+    
+#     from app import create_app,db
+# from app.models import *
+
+# from flask_migrate import Migrate, MigrateCommand
+# from flask_script import Manager,Shell,Server
+
+# # Creating app instance
+# app = create_app('development')
+
+# manager = Manager(app)
+# migrate = Migrate(app,db)
+
+# manager.add_command('db',MigrateCommand)
+# manager.add_command('server',Server)
+
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+
+# @manager.shell
+# def make_shell_context():
+#     return dict(app = app,db = db,User = User, Pitch=Pitch,Comment=Comment )
+
+# if __name__ == '__main__':
+#     manager.run()
